917dfe7b87dbc8252f45447fdc35a985a2d79a11
resembrink <suarezfc8@gmail.com>
2018-12-18 09:38:39 -0500

    modify

--- app/src/main/java/com/example/sistem03user/listchannelyoutube/Adapter/MyCustomAdapter.java
+++ app/src/main/java/com/example/sistem03user/listchannelyoutube/Adapter/MyCustomAdapter.java
@@ -1,12 +1,21 @@
 package com.example.sistem03user.listchannelyoutube.Adapter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.sistem03user.listchannelyoutube.MainActivity;
 import com.example.sistem03user.listchannelyoutube.Model.VideoDetails;
+import com.example.sistem03user.listchannelyoutube.R;
+import com.example.sistem03user.listchannelyoutube.VideoPlayActivity;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -16,6 +25,10 @@ public class MyCustomAdapter extends BaseAdapter
     Activity activity;
     ArrayList<VideoDetails> videoDetailsArrayList;
 
+    LayoutInflater inflater;
+
+
+
     public MyCustomAdapter(MainActivity activity, ArrayList<VideoDetails> videoDetailsArrayList) {
         this.activity=activity;
         this.videoDetailsArrayList = videoDetailsArrayList;
@@ -24,21 +37,61 @@ public class MyCustomAdapter extends BaseAdapter
 
     @Override
     public int getCount() {
-        return videoDetailsArrayList.size();
+        return this.videoDetailsArrayList.size();
     }
 
     @Override
-    public Object getItem(int position) {
+    public Object getItem(int position)
+    {
         return videoDetailsArrayList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return position;
+        return (long)position;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup viewGroup) {
+    public View getView(int position,  View convertView, ViewGroup viewGroup)
+    {
+
+        if(inflater==null)
+        {
+            inflater= this.activity.getLayoutInflater();
+        }
+
+        if(convertView==null)
+        {
+            convertView= inflater.inflate(R.layout.custom_item,null);
+        }
+
+        ImageView imageView= convertView.findViewById(R.id.imageView);
+        TextView textView= convertView.findViewById(R.id.mytitle);
+        LinearLayout linearLayout= (LinearLayout)convertView.findViewById(R.id.root);
+        final VideoDetails videoDetails= (VideoDetails) this.videoDetailsArrayList.get(position);
+
+
+
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(activity, VideoPlayActivity.class);
+                intent.putExtra("videoId", videoDetails.getVideoId());
+
+                activity.startActivity(intent);
+
+
+
+            }
+        });
+
+
+
+        Picasso.get().load(videoDetails.getUrl()).into(imageView);
+
+        textView.setText(videoDetails.getTitle());
+
+
         return convertView;
     }
 }
---
+++ app/src/main/java/com/example/sistem03user/listchannelyoutube/VideoPlayActivity.java
@@ -0,0 +1,13 @@
+package com.example.sistem03user.listchannelyoutube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_details);
+    }
+}
